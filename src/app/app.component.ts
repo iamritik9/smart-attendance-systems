b1e4e0b76010c3323294ce42284179398ccf7d5e
Roshit111 <roshit.yernagula@gmail.com>
2024-10-17 09:51:56 +0530

    Templete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page/landing-page.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'Smart-Attendance-System';
---
+++ src/app/components/breadcrumps/breadcrumps.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrumps',
+  standalone: true,
+  imports: [],
+  templateUrl: './breadcrumps.component.html',
+  styleUrl: './breadcrumps.component.css'
+})
+export class BreadcrumpsComponent {
+
+}
