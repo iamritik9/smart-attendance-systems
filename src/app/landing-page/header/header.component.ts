20f398ba1a23484866966458259944a927b2f698
Roshit111 <roshit.yernagula@gmail.com>
2024-10-19 17:15:49 +0530

    Header Update

--- src/app/landing-page/header/header.component.ts
+++ src/app/landing-page/header/header.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   logoPath: string = 'assets/favicon.png';
-  name: string="Smart Attendance System";
+  name: string = "Smart Attendance System";
 
   login() {
-    console.log('Login button clicked');
+    alert("Login Button Clicked");
   }
+
+  LinkDetails = [
+    {
+      Name: 'Home',
+      URL: "//",
+    },
+    {
+      Name: 'About',
+      URL: "//",
+    },
+    {
+      Name: 'Our Features',
+      URL: "//",
+    },
+    {
+      Name: 'Contact',
+      URL: "//",
+    }
+  ]
 }
