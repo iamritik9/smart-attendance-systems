2b4cccad5c6410298cb147906dd49dca4a381841
Roshit111 <roshit.yernagula@gmail.com>
2024-10-19 10:57:48 +0530

    Footer & Feature Update

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  ProjectName = "Smart Attendance System";
+  currentYear: number = new Date().getFullYear();
 
+  Details = [
+    {
+      Name: 'Roshit',
+      URL: 'https://www.linkedin.com/in/yernagula-roshit',
+      Div: "||"
+    },
+    {
+      Name: 'Manikanta',
+      URL: 'https://www.linkedin.com/in/manikanta',
+    }
+  ]
 }
