41cca64cea2c47625aa38fdf04caf5f06dd6ffd8
Roshit111 <roshit.yernagula@gmail.com>
2024-10-29 10:05:39 +0530

    Header & Footer Update

--- src/app/landing-page/footer/footer.component.ts
+++ src/app/landing-page/footer/footer.component.ts
@@ -9,49 +9,48 @@ import { CommonModule } from '@angular/common';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  ProjectName = "Smart Attendance System";
-  currentYear: number = new Date().getFullYear();
+  currentYear: number = new Date().getFullYear();;
+  projectName: string = "Smart Attendance System";
 
-  Details = [
+  personDetails = [
     {
-      Name : 'Roshit',
-      URL : 'https://www.linkedin.com/in/yernagula-roshit',
-      Div : '||',
+      Name: 'Roshit',
+      URL: 'https://www.linkedin.com/in/yernagula-roshit',
+      Div: '||',
     },
     {
-      Name : 'Manikanta',
-      URL : 'https://www.linkedin.com/in/palavalasa-manikanta-varaprasad-0b430a2a3/',
-      Div : '||',
+      Name: 'Manikanta',
+      URL: 'https://www.linkedin.com/in/palavalasa-manikanta-varaprasad-0b430a2a3/',
+      Div: '||',
     },
     {
-      Name : 'Ritik',
-      URL : 'https://www.linkedin.com/in/ritik-chandra-mishra/',
-      Div : '||',
+      Name: 'Ritik',
+      URL: 'https://www.linkedin.com/in/ritik-chandra-mishra/',
+      Div: '||',
     },
     {
-      Name : 'Silpi',
-      URL : 'https://www.linkedin.com/in/silpi-dash-660976291/',
-      Div : '||',
+      Name: 'Silpi',
+      URL: 'https://www.linkedin.com/in/silpi-dash-660976291/',
+      Div: '||',
     },
     {
       Name: 'Hari',
       URL: 'https://www.linkedin.com/in/kalisiti-srihari-a8184b315/',
-      Div : '||',
+      Div: '||',
     },
     {
-      Name:'Tarak',
-      URL:'https://www.linkedin.com/in/barnana-tarakeshwara-rao-b575ba300/',
-      Div : '||',
+      Name: 'Tarak',
+      URL: 'https://www.linkedin.com/in/barnana-tarakeshwara-rao-b575ba300/',
+      Div: '||',
     },
     {
-      Name:'Sankar',
-      URL:'https://www.linkedin.com/in/durgasi-shankar-rao-03a270300/',
-      Div : '||',
+      Name: 'Sankar',
+      URL: 'https://www.linkedin.com/in/durgasi-shankar-rao-03a270300/',
+      Div: '||',
     },
     {
-      Name:'Chandu',
-      URL:'https://www.linkedin.com/in/barnana-tarakeshwara-rao-b575ba300/',
-      Div : '||',
+      Name: 'Chandu',
+      URL: 'https://www.linkedin.com/in/barnana-tarakeshwara-rao-b575ba300/',
     }
   ]
 }
--- src/app/landing-page/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page/landing-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeComponent } from '../home/home.component';
-import { AboutComponent } from '../about/about.component';
 import { FeaturesComponent } from '../features/features.component';
 import { ContactComponent } from '../contact/contact.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -9,7 +8,7 @@ import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent,HomeComponent, AboutComponent, FeaturesComponent, ContactComponent, FooterComponent],
+  imports: [HeaderComponent,HomeComponent,  FeaturesComponent, ContactComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
